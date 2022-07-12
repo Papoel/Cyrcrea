19beae7b495b5858789143e8587c2ee401b3fccc
Papoel <pascal.briffard@gmail.com>
2022-07-12 21:24:58 +0200

    Rename Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProductController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('products/home.html.twig', [
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'ProductsController',
         ]);
     }
