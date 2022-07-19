81bda4ed01c1d247013583be2567e1ea7bc729fe
Papoel <pascal.briffard@gmail.com>
2022-07-19 23:57:29 +0200

    Création du formaulaire de détails du Compte

---
+++ src/Form/AccountDetailsFormType.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class AccountDetailsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => ['class' => 'text-muted text-uppercase'],
+                'attr' => ['placeholder' => 'Bruce'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez renseigner votre prénom.'
+                    ])
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => ['class' => 'text-muted text-uppercase'],
+                'attr' => ['placeholder' => 'Wayne'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez renseigner votre nom.'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'label_attr' => ['class' => 'text-muted text-uppercase'],
+                'attr' => ['placeholder' => 'batman@gotham.dc'],
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'label_attr' => ['class' => 'text-muted'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'label_attr' => ['class' => 'text-muted'],
+                ],
+
+                'invalid_message' => 'Les mots de passe doivent être identique',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length(
+                        [
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                            'maxMessage' => 'Votre mot de passe ne peut pas comporter plus de {{ limit }} caractères.',
+                        ]
+                    ),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
