1733b110d26f30497f732909772dcf227358e4bd
Papoel <pascal.briffard@gmail.com>
2022-08-19 14:53:21 +0200

    WIP: Affichage d'un produits et des offres spéciales etc...

--- src/Controller/Produit/ProductController.php
+++ src/Controller/Produit/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Produit;
 
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_index')]
-    public function index(ProductsRepository $productsRepository): Response
+    public function index(ProductsRepository $productsRepository, CategoriesRepository $categoriesRepository): Response
     {
 //        $product = $productsRepository->findOneBy(['id' => 3]);
 //
@@ -26,6 +27,17 @@ class ProductController extends AbstractController
 
         return $this->render('product/index.html.twig', [
             'produits' => $productsRepository->findAll(),
+            'categories' => $categoriesRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'app_product_show')]
+    public function show(Products $product, CategoriesRepository $categoriesRepository): Response
+    {
+        // TODO: Re-Factorisation général, ne passer les catégories que dans le Header...
+        return $this->render('product/show.html.twig', [
+            'produits' => $product,
+            'categories' => $categoriesRepository->findAll(),
         ]);
     }
 }
