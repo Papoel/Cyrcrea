137be3396a672c99064af204243b98c5c0a13deb
Papoel <pascal.briffard@gmail.com>
2022-08-24 14:56:43 +0200

    Création des cartes de produits

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('product-card', './assets/js/product-card.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
