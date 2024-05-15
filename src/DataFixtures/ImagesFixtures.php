a018b7aa2c49a95265f6142cc2ab8a01a0efaecf
Papoel <pascal.briffard@gmail.com>
2024-05-15 17:48:36 +0200

    reprise projet

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Random\RandomException;
 
 class ImagesFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -17,7 +18,11 @@ class ImagesFixtures extends Fixture implements DependentFixtureInterface
         for ($img = 1; $img <= 100; $img++) {
             $image = new Images();
             $image->setName($faker->image( null, 640, 380));
-            $product = $this->getReference('product'.random_int(1, 15));
+            try {
+                $product = $this->getReference('product' . random_int(1, 15));
+            } catch (RandomException $e) {
+                dump('Erreur Fixtures Images: ' .$e);
+            }
             $image->setProducts($product);
 
             //dump($image);
